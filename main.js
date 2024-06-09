29e29ee8072143bc8b1c29af7e635fee3097a8f5
azykdev <azyk0106@gmail.com>
2024-06-09 18:06:28 +0500

    complete

--- main.js
+++ main.js
@@ -1,2 +1,49 @@
 "use strict";
-console.log('Hello, Snowpack!');
+const deleteAll = document.querySelector('.delete-all');
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoList = document.querySelector('.todo-list');
+let allTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+// Functions
+const writeTodos = (todos) => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+    todoList.innerHTML = '';
+    todos.forEach((todo) => {
+        var _a, _b;
+        const newTodo = document.createElement('li');
+        newTodo.className = 'todo-item';
+        newTodo.innerHTML = `
+      <input type="checkbox" class="todo-checkbox" ${todo.completed ? 'checked' : ''}>
+      <span class="todo-text">${todo.text}</span>
+      <button class="delete-todo">Delete</button>
+    `;
+        (_a = newTodo.querySelector('.delete-todo')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
+            allTodos = allTodos.filter(t => t.id !== todo.id);
+            writeTodos(allTodos);
+        });
+        (_b = newTodo.querySelector('.todo-checkbox')) === null || _b === void 0 ? void 0 : _b.addEventListener('change', (e) => {
+            todo.completed = e.target.checked;
+            writeTodos(allTodos);
+        });
+        todoList.appendChild(newTodo);
+    });
+};
+// Events
+window.addEventListener('DOMContentLoaded', () => {
+    writeTodos(allTodos);
+});
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newTodo = {
+        id: Date.now(),
+        text: todoInput.value,
+        completed: false
+    };
+    allTodos.push(newTodo);
+    writeTodos(allTodos);
+    todoInput.value = '';
+});
+deleteAll.addEventListener('click', () => {
+    allTodos.length = 0;
+    writeTodos(allTodos);
+});
--- main.ts
+++ main.ts
@@ -1,1 +1,70 @@
-console.log('Hello, Snowpack!');
+// Interface
+interface ITodo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const deleteAll = document.querySelector('.delete-all') as HTMLButtonElement;
+const todoForm = document.querySelector('.todo-form') as HTMLFormElement;
+const todoInput = document.querySelector('.todo-input') as HTMLInputElement;
+const todoList = document.querySelector('.todo-list') as HTMLUListElement;
+
+let allTodos: ITodo[] = JSON.parse(localStorage.getItem('todos') || '[]');
+
+// Functions
+const writeTodos = (todos: ITodo[]): void => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+  todoList.innerHTML = '';
+
+  todos.forEach((todo: ITodo): void => {
+    const newTodo = document.createElement('li');
+    newTodo.className = 'todo-item';
+
+    newTodo.innerHTML = `
+      <input type="checkbox" class="todo-checkbox" ${todo.completed ? 'checked' : ''}>
+      <span class="todo-text">${todo.text}</span>
+      <button class="delete-todo">Delete</button>
+    `;
+
+    newTodo.querySelector('.delete-todo')?.addEventListener('click', () => {
+      allTodos = allTodos.filter(t => t.id !== todo.id);
+      writeTodos(allTodos);
+    });
+
+    newTodo.querySelector('.todo-checkbox')?.addEventListener('change', (e: Event) => {
+      todo.completed = (e.target as HTMLInputElement).checked;
+      writeTodos(allTodos);
+    });
+
+    todoList.appendChild(newTodo);
+  });
+}
+
+// Events
+window.addEventListener('DOMContentLoaded', (): void => {
+  writeTodos(allTodos);
+});
+
+todoForm.addEventListener('submit', (e: Event): void => {
+  e.preventDefault();
+
+  const newTodo: ITodo = {
+    id: Date.now(),
+    text: todoInput.value,
+    completed: false
+  };
+
+  allTodos.push(newTodo);
+
+  writeTodos(allTodos);
+
+  todoInput.value = '';
+});
+
+deleteAll.addEventListener('click', (): void => {
+
+  allTodos.length = 0;
+
+  writeTodos(allTodos);
+});
